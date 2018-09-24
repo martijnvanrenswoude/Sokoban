cfdaf36c8bfd7f638fbcb304f80a4a36ac87e75b
Wessel <wjfuhr@avans.nl>
2018-09-24 13:56:41 +0200

    input v1

--- InputView.cs
+++ InputView.cs
@@ -7,5 +7,24 @@ namespace Sokoban
 {
     public class InputView
     {
+        public int ReadLevelInput()
+        {
+            String input = Console.ReadLine();
+            int level = -1;
+            try{
+                level = int.Parse(input);
+            }catch (Exception e)
+            {
+                Console.WriteLine("Voer aub een getal in");
+            }
+            if (level > 0 && level <= 4)
+            {
+                return level;
+            }
+            else
+            {
+                Console.WriteLine("kies aub een valide level");
+            }
+        }
     }
 }
