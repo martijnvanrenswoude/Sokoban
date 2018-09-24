c6af657ca43caea3824cd6710934ef1b983ef32c
Wessel <wjfuhr@avans.nl>
2018-09-24 13:38:28 +0200

    output v1
    yeet

--- OutputView.cs
+++ OutputView.cs
@@ -7,5 +7,27 @@ namespace Sokoban
 {
     public class OutputView
     {
+
+        public void showTutuorial()
+        {
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("| Welkom bij Sokoban                                |");
+            Console.WriteLine("|                                                   |");
+            Console.WriteLine("| betekenis van de symbolen   |  doel van het spel  |");
+            Console.WriteLine("| spatie : outerspace         |                     |");
+            Console.WriteLine("|      # : muur               |  duw met de truck   |");
+            Console.WriteLine("|      . : vloer              |  de krat(ten)       |");
+            Console.WriteLine("|      o : krat               |  naar de bestemming |");
+            Console.WriteLine("|      0 : krat op bestemming |                     |");
+            Console.WriteLine("|      x : bestemming         |                     |");
+            Console.WriteLine("|      @ : truck              |                     |");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("");
+        }
+
+        public void showLevelPicker()
+        {
+            Console.WriteLine("> Kies een doolhof (1-4), s = stop");
+        }
     }
 }
