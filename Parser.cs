b6c949e3421fadf1c829f46164e7e6d4ea3ed49c
martijnvanrenswoude <mrenswou@avans.nl>
2018-09-25 13:30:50 +0200

    k

--- Parser.cs
+++ Parser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace Sokoban
 {
@@ -11,6 +12,39 @@ namespace Sokoban
         public Parser()
         {
 
+        }
+
+        public void doParsing(int levelSelection)
+        {
+            string filepath = @"C:\Users\Martijn\Documents\GitHub\Sokoban\Doolhof\doolhof" + levelSelection + ".txt";
+            char[] maze = File.ReadAllText(filepath).ToCharArray();
+
+            foreach(char square in maze)
+            {
+                if (square == '#')
+                {
+                    //krijgt in constructor nog character mee, voor de zekerheid
+                    Wall wall = new Wall();
+                }
+                else if (square == '.')
+                {
+                    Floor floor = new Floor();
+                }
+                else if (square == 'x')
+                {
+                    Destination destination = new Destination();
+                }
+                else if (square == 'o')
+                {
+                    Chest chest = new Chest();
+                }
+                else if(square == '@')
+                {
+                    Player player = new Player();
+                }
+
+            }
+            
         }
 
         public char[][] getLocationArray()
--- PlayField.cs
+++ PlayField.cs
@@ -7,12 +7,47 @@ namespace Sokoban
 {
     public class PlayField
     {
+        public char[] Maze { get; set; }
 
+        public PlayField(char[] maze)
+        {
+            Maze = maze;
+        }
 
 
         private void setLocations(int height, int width, char[][] loc)
         {
             
+        }
+
+        private void initializeGameObjects()
+        {
+
+            foreach (char square in Maze)
+            {
+                if (square == '#')
+                {
+                    //krijgt in constructor nog character mee, voor de zekerheid
+                    Wall wall = new Wall();
+                }
+                else if (square == '.')
+                {
+                    Floor floor = new Floor();
+                }
+                else if (square == 'x')
+                {
+                    Destination destination = new Destination();
+                }
+                else if (square == 'o')
+                {
+                    Chest chest = new Chest();
+                }
+                else if (square == '@')
+                {
+                    Player player = new Player();
+                }
+
+            }
         }
 
 
