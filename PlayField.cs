29d2f64dd5181931a64005e83049d6709209e974
martijnvanrenswoude <mrenswou@avans.nl>
2018-10-03 10:57:01 +0200

    Frits Wester

--- PlayField.cs
+++ PlayField.cs
@@ -9,7 +9,7 @@ namespace Sokoban
     {
         public Parser Parser { get; private set; }
         public Floor[,] ground { get; set; }
-        public Floor[] floor { get; set; }
+        public Floor[] floors { get; set; }
         public Floor First { get; set; }
 
         public PlayField(Parser parser)
@@ -21,57 +21,57 @@ namespace Sokoban
 
         private void setLocations(int height, int width, char[][] loc)
         {
-            
-        }
-
-        private void initField()
-        {
-
-            foreach (string temp in Maze)
-            {
-
-                for (int i = 0; i < Maze.Length; i++)
-                {
-                    for (int j = 0; j < Maze[j].Length; j++)
-                    {
-                        if (ground == null)
-                        {
-                            ground = new Floor[Maze.Length, Maze[i].Length];
-                        }
-                        char tempCharacter = Maze[i][j];
-
-                        Floor tempGround = ground[Maze.Length, Maze[i].Length] = new Floor();
-                        tempGround.GameObject = createGameObject(tempCharacter);
-                    }
-                }
-            }
 
         }
 
-        public void initFieldPartTwo()
-        {
-            Floor tempHorizontal = First, tempVertical = First;
+        //private void initField()
+        //{
+
+        //    foreach (string temp in Maze)
+        //    {
+
+        //        for (int i = 0; i < Maze.Length; i++)
+        //        {
+        //            for (int j = 0; j < Maze[j].Length; j++)
+        //            {
+        //                if (ground == null)
+        //                {
+        //                    ground = new Floor[Maze.Length, Maze[i].Length];
+        //                }
+        //                char tempCharacter = Maze[i][j];
+
+        //                Floor tempGround = ground[Maze.Length, Maze[i].Length] = new Floor();
+        //                tempGround.GameObject = createGameObject(tempCharacter);
+        //            }
+        //        }
+        //    }
+
+        //}
+
+        //public void initFieldPartTwo()
+        //{
+        //    Floor tempHorizontal = First, tempVertical = First;
 
 
 
-            for(int i = 0; i < Maze.Length; i++)
-            {
-                if(i > 0)
-                {
-                  
-                }
+        //    for(int i = 0; i < Maze.Length; i++)
+        //    {
+        //        if(i > 0)
+        //        {
 
-                for(int j = 0; j < Maze[i].Length; j++)
-                {
-                    tempHorizontal.East = new Floor();
-                    tempHorizontal.East.West = tempHorizontal;
-                    tempHorizontal = tempHorizontal.East;
+        //        }
+
+        //        for(int j = 0; j < Maze[i].Length; j++)
+        //        {
+        //            tempHorizontal.East = new Floor();
+        //            tempHorizontal.East.West = tempHorizontal;
+        //            tempHorizontal = tempHorizontal.East;
 
 
-                }
-            }
+        //        }
+        //    }
 
-        }
+        //}
 
         private GameObject createGameObject(char type)
         {
@@ -80,9 +80,6 @@ namespace Sokoban
                 case '#':
                     Wall wall = new Wall('#');
                     return wall;
-                case '.':
-                    Floor floor = new Floor('.');
-                    return floor;
                 case '@':
                     Player player = new Player('@');
                     return player;
@@ -97,19 +94,109 @@ namespace Sokoban
                     return null;
             }
 
-        }
 
+
+        }
         private void makeItems()
         {
-            floor = new Floor[parser.numberOfItems()]; //maak de array
-            for (int i = 0; i < floor.Length; i++)         // vul de array met objecten
+            floors = new Floor[Parser.getNumberOfItems()]; //maak de array
+            for (int i = 0; i < floors.Length; i++)         // vul de array met objecten
             {
-                floor[i] = new Floor();
+                floors[i] = new Floor();
             }
         } // maak het item array
 
+        private void setAllRows()
+        {
+            for (int i = 0; i < Parser.getNumberOfRows(); i++)
+            {
+                setRow(i, Parser.getDataWithIndex(i));
+            }
+        } //loop door alle rows heen en vul die in
 
+        private void setRow(int rowNumber, String value)
+        {
+            int index; //maak een index aan voor de vertaling naar het array
+            for (int i = 0; i < value.Length; i++) //de lengte van de String is de grootte van een rij
+            {
+                index = CalcultateIndex(i, rowNumber);  // vertaal de coordinaat naar een index
+                floors[index].setContent(value[i]);      // set de juiste waarde in het item
+                //vul de referenties\\
+                //set north
+                setNorth(rowNumber, i, index);
+                //set south
+                setSouth(rowNumber, i, index);
+                //set west
+                setWest(rowNumber, i, index);
+                //set east
+                setEast(rowNumber, i, index);
+            }
+        } //vul een rij in
 
+        private void setNorth(int row, int column, int index)
+        {
+            if (row > 0)
+            {
+                floors[index].North = floors[CalcultateIndex(column, row - 1)];
+            }
+            else
+            {
+                floors[index].North = null;
+            }
+        } //set de noord referentie van een Link
 
+        private void setSouth(int row, int column, int index)
+        {
+            if (row < Parser.getNumberOfRows() - 1)
+            {
+                floors[index].South = floors[CalcultateIndex(column, row + 1)];
+            }
+            else
+            {
+                floors[index].South = null;
+            }
+        } //set de zuid referentie van een Link
+
+        private void setWest(int row, int column, int index)
+        {
+            if (column > 0)
+            {
+                floors[index].West = floors[CalcultateIndex(column - 1, row)];
+            }
+            else
+            {
+                floors[index].West = null;
+            }
+        } //set de west referentie van een Link
+
+        private void setEast(int row, int column, int index)
+        {
+            if (column < Parser.getNumberOfRows() - 1)
+            {
+                floors[index].East = floors[CalcultateIndex(column + 1, row)];
+            }
+            else
+            {
+                floors[index].East = null;
+            }
+        } //set de oost referentie van een Link
+
+        private int CalcultateIndex(int column, int row)
+        {
+            return column + (row * Parser.getNumberColumn());
+        } //convert de kolom en de rij naar de index in het item array
+
+        public void print()
+        {
+            for (int i = 0; i < Parser.getNumberOfRows(); i++)
+            {
+                for (int j = 0; j < Parser.getNumberColumn(); j++)
+                {
+                    Console.Write(floors[CalcultateIndex(j, i)].GetType());
+                }
+                Console.WriteLine();
+            }
+        }//print de data uit het item array
     }
+
 }
