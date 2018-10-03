99f11cd0da1f9f7c44734fc7503fe38d7fd9e132
martijnvanrenswoude <mrenswou@avans.nl>
2018-10-03 14:25:12 +0200

    gegergergwr

--- Player.cs
+++ Player.cs
@@ -17,14 +17,65 @@ namespace Sokoban
         public override void move(String direction)
         {
             
+            switch (direction)
+            {
+                case "links" :
+                    if(floor.West == null || !(floor.West.getGameObjectType().Equals('#')))
+                    {
+                        swap(direction);
+
+                    }
+                    break;
+                case "rechts":
+                    if (floor.East == null || !(floor.East.getGameObjectType().Equals('#')))
+                    {
+                        swap(direction);
+
+                    }
+                    break;
+
+                case "omhoog":
+                    if (floor.North == null || !(floor.North.getGameObjectType().Equals('#')))
+                    {
+                        swap(direction);
+
+                    }
+                    break;
+
+                case "omlaag":
+                    if (floor.South == null || !(floor.South.getGameObjectType().Equals('#')))
+                    {
+                        swap(direction);
+
+                    }
+                    break;
+            }
         }
 
         public override void swap(string direction)
         {
             switch (direction)
             {
-                //case "links": 
-
+                case "links":
+                    floor.West.GameObject = this;
+                    floor.GameObject = null;
+                    floor = floor.West;
+                    break;
+                case "rechts":
+                    floor.East.GameObject = this;
+                    floor.GameObject = null;
+                    floor = floor.East;
+                    break;
+                case "omhoog":
+                    floor.North.GameObject = this;
+                    floor.GameObject = null;
+                    floor = floor.North;
+                    break;
+                case "omlaag":
+                    floor.South.GameObject = this;
+                    floor.GameObject = null;
+                    floor = floor.South;
+                    break;
             }
         }
 
